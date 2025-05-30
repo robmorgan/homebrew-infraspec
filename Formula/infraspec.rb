4816a2d22ba6f4f31b0e8af79a4867a3c4ef7197
Rob Morgan <robbym@gmail.com>
2025-05-30 17:15:26 +0800

    chore: tweak formula

--- Formula/infraspec.rb
+++ Formula/infraspec.rb
@@ -1,7 +1,7 @@
 class Infraspec < Formula
   desc "Test your cloud infrastructure in plain English, no code required."
   homepage "https://infraspec.sh"
-  url "https://github.com/robmorgan/infraspec/archive/v0.0.4.tar.gz"
+  url "https://github.com/robmorgan/infraspec/archive/refs/tags/v0.0.4.tar.gz"
   sha256 "4218acf668c060633184d4b7519be7bc49ee233ef5abd4378f2eb4cf223e1ff5"
   license "FCL-1.0-ALv2"
   version "0.0.4"
@@ -15,15 +15,23 @@ class Infraspec < Formula
   depends_on "go" => :build
 
   def install
-    ldflags = %W[
-      -s -w
-      -X github.com/robmorgan/infraspec/cmd.Version=#{version}
-    ]
+    infraspec_version = if build.stable?
+      version.to_s
+    else
+      Utils.safe_popen_read("git", "describe", "--tags", "--dirty").chomp
+    end
 
-    system "go", "build", *std_go_args(ldflags:), "cmd"
+    with_env(
+      "INFRASPEC_VERSION"   => infraspec_version,
+      "GO_LDFLAGS"   => "-s -w",
+      "GO_BUILDTAGS" => "updateable",
+    ) do
+      system "go", "run", "./script/build.go"
+    end
+    bin.install "bin/infraspec"
   end
 
   test do
-    system "#{bin}/infraspec", "--version"
+    assert_match "infraspec version #{version}", shell_output("#{bin}/infraspec --version")
   end
 end
