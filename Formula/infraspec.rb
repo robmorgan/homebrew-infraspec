4cb6a19e080c00541d2a06214cb5820e2f828308
Rob Morgan <robbym@gmail.com>
2025-05-30 16:27:26 +0800

    feat: add livecheck

--- Formula/infraspec.rb
+++ Formula/infraspec.rb
@@ -2,11 +2,15 @@ class Infraspec < Formula
   desc "Test your cloud infrastructure in plain English, no code required."
   homepage "https://infraspec.sh"
   url "https://github.com/robmorgan/infraspec/archive/v0.0.4.tar.gz"
-
   sha256 "4218acf668c060633184d4b7519be7bc49ee233ef5abd4378f2eb4cf223e1ff5"
   license "FCL-1.0-ALv2"
   version "0.0.4"
   head "https://github.com/robmorgan/infraspec.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   depends_on "go" => :build
 
